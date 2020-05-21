f50297881df91d75985774025b5523077c7dcfb8
Amandeep <sharma67@buffalo.edu>
2020-05-21 12:14:24 -0500

    function to get the list of lines for each card

--- dyna/funcs.py
+++ dyna/funcs.py
@@ -4,16 +4,16 @@ def find_key(file: list, key: str):
     2. key: str of key
     """
     # define empty list for tuples of start and end
-    num = []
-    # define empty list to add all the matches found
-    dup = []
+    num_list = []
+
     # iterate over the lines in file (file is list from f.readlines()
     for line_num, line in enumerate(file):
         # if the key is in the line do following
-        if key in line:
-            # num.append(line_num)
-            dup.append("found")
-            # take the slice of the file starting from that line+1
+        if key == line:
+            num = []
+            line_list = []
+
+                       # take the slice of the file starting from that line+1
             slice = file[line_num + 1:]
 
             # loop over slice
@@ -21,7 +21,10 @@ def find_key(file: list, key: str):
                 # if * is in the slice means start of new dyna
                 if "*" not in line_:
                     num.append(line_num + line_num_ + 1)
+                    line_list.append(line_)
                 else:
                     break
-        # print(i)
-    return num, len(num)
+
+            num_list.append(line_list)
+
+    return num_list
